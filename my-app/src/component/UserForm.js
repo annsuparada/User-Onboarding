b7623009bdfb629c18bb3f986ff5f5a0a4129bfe
Suparada Saitalae <ann.suparada@gmail.com>
2019-08-07 15:17:31 -0500

    STEP 2 - Implement Form Validation and Error Messaging DONE!

--- my-app/src/component/UserForm.js
+++ my-app/src/component/UserForm.js
@@ -3,14 +3,23 @@ import axios from 'axios';
 import { Form, Field, withFormik } from 'formik';
 import * as Yup from 'yup';
 
-const UserForm = ({ name, email, password, termsOfServices, values }) => {
+const UserForm = ({ touched, errors, values }) => {
     return(
         <div>
             <h2>This is a Form</h2>
             <Form>
                 <Field type="text" name="name" placeholder="Name" />
+                {touched.name && errors.name && (
+                    <p>{errors.name}</p>
+                )}
                 <Field type="text" name="email" placeholder="Email" />
+                {touched.email && errors.email && (
+                    <p>{errors.email}</p>
+                )}
                 <Field type="text" name="password" placeholder="Password" />
+                {touched.password && errors.password && (
+                    <p>{errors.password}</p>
+                )}
                 <lable>
                     Terms of Service
                     <Field
@@ -34,8 +43,16 @@ const FormikUserForm = withFormik({
             termsOfServices: termsOfServices || '',
            
 
-        }
-    }
+        };
+    },
+
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required(),
+        email: Yup.string().required(),
+        password: Yup.string().required(),
+        termsOfServices: Yup.string().required()
+    })
+
 })(UserForm)
 
 export default FormikUserForm;
