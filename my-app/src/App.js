e8b360a56d1a3e8a18d2de0d3ea663872bdbb025
Suparada Saitalae <ann.suparada@gmail.com>
2019-08-07 14:42:12 -0500

    first commit, create FormUser.js file

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import FormUser from './component/FormUser'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>User Onborading</h1>
+      </header>
+      <FormUser />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/component/FormUser.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Form, Field, withFormik } from 'formik';
+import * as Yup from 'yup';
+
+const FormUser = () => {
+    return(
+        <div>
+            <h2>This is a Form</h2>
+        </div>
+    )
+}
+
+export default FormUser;
