d5fdead631fa255c5793e41635c19fcaec06880b
Suparada Saitalae <ann.suparada@gmail.com>
2019-08-07 19:08:59 -0500

    styling

--- my-app/src/component/UserForm.js
+++ my-app/src/component/UserForm.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import { Form, Field, withFormik } from 'formik';
 import * as Yup from 'yup';
 
+
+
 const UserForm = ({ touched, errors, values, handleSubmit, status }) => {
     const [users, setUsers] = useState([])
     
@@ -13,34 +15,37 @@ const UserForm = ({ touched, errors, values, handleSubmit, status }) => {
     }, [status])
 
     return(
-        <div className="users-form">
-            <h2>This is a Form</h2>
-            <Form>
-                <Field type="text" name="name" placeholder="Name" /> <br/>
-                {touched.name && errors.name && (
-                    <p className="error">{errors.name}</p>
-                )}
-                <Field type="text" name="email" placeholder="Email" /> <br/>
-                {touched.email && errors.email && (
-                    <p className="error">{errors.email}</p>
-                )}
-                <Field type="text" name="password" placeholder="Password" /> <br/>
-                {touched.password && errors.password && (
-                    <p className="error">{errors.password}</p>
-                )}
-                <label>
-                    Terms of Service
-                    <Field
-                        type="checkbox"
-                        name="termsOfServices"
-                        checked={values.termsOfServices}
-                    />
-                </label>
-                <button type="submit">Submit</button>
-            </Form>
-           {users.map(user => (
-               <p key={user.id}>{user.name}</p>
-               ))}
+        <div className="dashboard">
+            <div className="users-form">
+                <h2>This is a Form</h2>
+                <Form>
+                    <Field type="text" name="name" placeholder="Name" /> <br/>
+                    {touched.name && errors.name && (
+                        <p className="error">{errors.name}</p>
+                    )}
+                    <Field type="text" name="email" placeholder="Email" /> <br/>
+                    {touched.email && errors.email && (
+                        <p className="error">{errors.email}</p>
+                    )}
+                    <Field type="text" name="password" placeholder="Password" /> <br/>
+                    {touched.password && errors.password && (
+                        <p className="error">{errors.password}</p>
+                    )}
+                    <label>
+                        Terms of Service
+                        <Field
+                            type="checkbox"
+                            name="termsOfServices"
+                            checked={values.termsOfServices}
+                        />
+                    </label>
+                    <button type="submit">Submit</button>
+                </Form>
+            
+            </div>
+            {users.map(user => (
+                <h3 key={user.id}>{user.name}</h3>
+                ))}
         </div>
     )
 }
